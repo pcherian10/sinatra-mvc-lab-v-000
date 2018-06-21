f25840a6ab74960a563509a4f0f0c1c0994e4d99
Paul <pcherian10@gmail.com>
2018-06-21 04:08:02 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,17 +9,12 @@ class PigLatinizer
     @text.split(" ")
 
     @text.collect do |word|
-      
-    if"bcdfghjklmnpqrstvwxyz".split("").include? (text[0])
-      word[(1..-1)] + word[0]+"ay"
+      if"bcdfghjklmnpqrstvwxyz".split("").include? (text[0])
+        word[(1..-1)] + word[0]+"ay"
+      elsif"aeiou".split("").include? (text[0])
+        word +"ay"
+      elsif !("aeiou").split("").include? (text[0] && text[1])
 
-    elsif"aeiou".split("").include? (text[0])
-      word +"ay"
-
-    elsif !("aeiou").split("").include? (text[0] && text[1])
-
-
-    elsif
 
 
 end
