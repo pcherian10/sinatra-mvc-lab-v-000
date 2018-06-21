fa4bed884f150c2e3851b5563a441c219029050e
Paul <pcherian10@gmail.com>
2018-06-21 02:54:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    @pig_latinzed = PigLatinizer.new(params[:user_text])
+
+    erb :results
+  end
+
 end
