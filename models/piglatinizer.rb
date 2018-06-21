ec22b6921cf162017c0c899dfeaa5831b6b61cab
Paul <pcherian10@gmail.com>
2018-06-21 04:07:41 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,13 +7,14 @@ class PigLatinizer
 
   def latinize
     @text.split(" ")
-    
-    
-    if"bcdfghjklmnpqrstvwxyz".split("").include? (text[0])
-      @text[(1..-1)] + @text[0]+"ay"
 
-    if"aeiou".split("").include? (text[0])
-      @text +"ay"
+    @text.collect do |word|
+      
+    if"bcdfghjklmnpqrstvwxyz".split("").include? (text[0])
+      word[(1..-1)] + word[0]+"ay"
+
+    elsif"aeiou".split("").include? (text[0])
+      word +"ay"
 
     elsif !("aeiou").split("").include? (text[0] && text[1])
 
